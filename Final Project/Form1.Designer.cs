38f2281e2e413c8c1ba78f2899d8d6e5a9e6ba16
Doak120 <101018255+Doak120@users.noreply.github.com>
2022-04-10 23:18:46 -0500

    Add project files.

---
+++ Final Project/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿
+namespace Final_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mainMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.superHeroBackStoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ironManToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backStoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.captionMarvelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backStoryToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripTextBox();
+            this.captionAmericaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backStoryToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox3 = new System.Windows.Forms.ToolStripTextBox();
+            this.spiderManToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backStoryToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox4 = new System.Windows.Forms.ToolStripTextBox();
+            this.picturesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comicsStoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tVShowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multyverseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainMenuToolStripMenuItem,
+            this.multyverseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // mainMenuToolStripMenuItem
+            // 
+            this.mainMenuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.superHeroBackStoriesToolStripMenuItem,
+            this.picturesToolStripMenuItem,
+            this.comicsStoriesToolStripMenuItem,
+            this.moviesToolStripMenuItem,
+            this.tVShowsToolStripMenuItem});
+            this.mainMenuToolStripMenuItem.Name = "mainMenuToolStripMenuItem";
+            this.mainMenuToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.mainMenuToolStripMenuItem.Text = "Marvel Heros";
+            // 
+            // superHeroBackStoriesToolStripMenuItem
+            // 
+            this.superHeroBackStoriesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ironManToolStripMenuItem,
+            this.captionMarvelToolStripMenuItem,
+            this.captionAmericaToolStripMenuItem,
+            this.spiderManToolStripMenuItem});
+            this.superHeroBackStoriesToolStripMenuItem.Name = "superHeroBackStoriesToolStripMenuItem";
+            this.superHeroBackStoriesToolStripMenuItem.Size = new System.Drawing.Size(251, 26);
+            this.superHeroBackStoriesToolStripMenuItem.Text = "Super Hero back Stories";
+            this.superHeroBackStoriesToolStripMenuItem.Click += new System.EventHandler(this.superHeroBackStoriesToolStripMenuItem_Click);
+            // 
+            // ironManToolStripMenuItem
+            // 
+            this.ironManToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backStoryToolStripMenuItem,
+            this.toolStripTextBox1});
+            this.ironManToolStripMenuItem.Name = "ironManToolStripMenuItem";
+            this.ironManToolStripMenuItem.Size = new System.Drawing.Size(207, 26);
+            this.ironManToolStripMenuItem.Text = "Iron Man";
+            // 
+            // backStoryToolStripMenuItem
+            // 
+            this.backStoryToolStripMenuItem.Name = "backStoryToolStripMenuItem";
+            this.backStoryToolStripMenuItem.Size = new System.Drawing.Size(174, 26);
+            this.backStoryToolStripMenuItem.Text = "Back Story";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 27);
+            this.toolStripTextBox1.Click += new System.EventHandler(this.toolStripTextBox1_Click);
+            // 
+            // captionMarvelToolStripMenuItem
+            // 
+            this.captionMarvelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backStoryToolStripMenuItem1,
+            this.toolStripTextBox2});
+            this.captionMarvelToolStripMenuItem.Name = "captionMarvelToolStripMenuItem";
+            this.captionMarvelToolStripMenuItem.Size = new System.Drawing.Size(207, 26);
+            this.captionMarvelToolStripMenuItem.Text = "Caption Marvel ";
+            this.captionMarvelToolStripMenuItem.Click += new System.EventHandler(this.captionMarvelToolStripMenuItem_Click);
+            // 
+            // backStoryToolStripMenuItem1
+            // 
+            this.backStoryToolStripMenuItem1.Name = "backStoryToolStripMenuItem1";
+            this.backStoryToolStripMenuItem1.Size = new System.Drawing.Size(174, 26);
+            this.backStoryToolStripMenuItem1.Text = "Back Story";
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.Size = new System.Drawing.Size(100, 27);
+            // 
+            // captionAmericaToolStripMenuItem
+            // 
+            this.captionAmericaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backStoryToolStripMenuItem2,
+            this.toolStripTextBox3});
+            this.captionAmericaToolStripMenuItem.Name = "captionAmericaToolStripMenuItem";
+            this.captionAmericaToolStripMenuItem.Size = new System.Drawing.Size(207, 26);
+            this.captionAmericaToolStripMenuItem.Text = "Caption America ";
+            // 
+            // backStoryToolStripMenuItem2
+            // 
+            this.backStoryToolStripMenuItem2.Name = "backStoryToolStripMenuItem2";
+            this.backStoryToolStripMenuItem2.Size = new System.Drawing.Size(174, 26);
+            this.backStoryToolStripMenuItem2.Text = "Back Story";
+            // 
+            // toolStripTextBox3
+            // 
+            this.toolStripTextBox3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox3.Name = "toolStripTextBox3";
+            this.toolStripTextBox3.Size = new System.Drawing.Size(100, 27);
+            // 
+            // spiderManToolStripMenuItem
+            // 
+            this.spiderManToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backStoryToolStripMenuItem3,
+            this.toolStripTextBox4});
+            this.spiderManToolStripMenuItem.Name = "spiderManToolStripMenuItem";
+            this.spiderManToolStripMenuItem.Size = new System.Drawing.Size(207, 26);
+            this.spiderManToolStripMenuItem.Text = "Spider Man ";
+            // 
+            // backStoryToolStripMenuItem3
+            // 
+            this.backStoryToolStripMenuItem3.Name = "backStoryToolStripMenuItem3";
+            this.backStoryToolStripMenuItem3.Size = new System.Drawing.Size(174, 26);
+            this.backStoryToolStripMenuItem3.Text = "Back Story";
+            this.backStoryToolStripMenuItem3.Click += new System.EventHandler(this.backStoryToolStripMenuItem3_Click);
+            // 
+            // toolStripTextBox4
+            // 
+            this.toolStripTextBox4.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox4.Name = "toolStripTextBox4";
+            this.toolStripTextBox4.Size = new System.Drawing.Size(100, 27);
+            // 
+            // picturesToolStripMenuItem
+            // 
+            this.picturesToolStripMenuItem.Name = "picturesToolStripMenuItem";
+            this.picturesToolStripMenuItem.Size = new System.Drawing.Size(251, 26);
+            this.picturesToolStripMenuItem.Text = "Pictures ";
+            // 
+            // comicsStoriesToolStripMenuItem
+            // 
+            this.comicsStoriesToolStripMenuItem.Name = "comicsStoriesToolStripMenuItem";
+            this.comicsStoriesToolStripMenuItem.Size = new System.Drawing.Size(251, 26);
+            this.comicsStoriesToolStripMenuItem.Text = "Comics Stories";
+            // 
+            // moviesToolStripMenuItem
+            // 
+            this.moviesToolStripMenuItem.Name = "moviesToolStripMenuItem";
+            this.moviesToolStripMenuItem.Size = new System.Drawing.Size(251, 26);
+            this.moviesToolStripMenuItem.Text = "Movies ";
+            // 
+            // tVShowsToolStripMenuItem
+            // 
+            this.tVShowsToolStripMenuItem.Name = "tVShowsToolStripMenuItem";
+            this.tVShowsToolStripMenuItem.Size = new System.Drawing.Size(251, 26);
+            this.tVShowsToolStripMenuItem.Text = "TV Shows";
+            // 
+            // multyverseToolStripMenuItem
+            // 
+            this.multyverseToolStripMenuItem.Name = "multyverseToolStripMenuItem";
+            this.multyverseToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
+            this.multyverseToolStripMenuItem.Text = "Multyverse ";
+            this.multyverseToolStripMenuItem.Click += new System.EventHandler(this.multyverseToolStripMenuItem_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(499, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(289, 370);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send to output ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Read lines";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 331);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(367, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Press for pictures";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(50, 106);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Random Number";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(205, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem mainMenuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem superHeroBackStoriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ironManToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem captionMarvelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backStoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backStoryToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem captionAmericaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backStoryToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem spiderManToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backStoryToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem multyverseToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem picturesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comicsStoriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moviesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tVShowsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox2;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox3;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Final Project/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Final_Project
+{
+    public partial class Form1 : Form
+    {
+        string textFile = "readfile.txt";
+        Image[] images = new Image[4];
+        int randomNum;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picturesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void captionMarvelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void superHeroBackStoriesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multyverseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backStoryToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var writer = new StreamWriter(@"output.txt"))
+                writer.WriteLine(textBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(textFile);
+            using(StreamReader file = new StreamReader(textFile))
+                foreach(string line in lines)
+                {
+                    MessageBox.Show(line);
+                }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            images[0] = Image.FromFile("c:/Users/Owner/Source/repos/Final Project/bin/Captain America.JFIF");
+            images[1] = Image.FromFile("c:/Users/Owner/Source/repos/Final Project/bin/IronMan3.0.JPG");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = 4;
+            Random random = new Random();
+            randomNum = random.Next(1,30);
+            textBox2.Text = randomNum.ToString();
+
+        }
+    }
+}
